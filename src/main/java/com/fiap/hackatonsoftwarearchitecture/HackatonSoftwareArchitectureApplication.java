fbf6a791aff7f843a7fdaeae705bcd569742c01b
Julia Pereira da Costa <juliapcosta97@gmail.com>
2024-03-20 22:12:19 -0300

    Init project

---
+++ src/main/java/com/fiap/hackatonsoftwarearchitecture/HackatonSoftwareArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackatonsoftwarearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonSoftwareArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonSoftwareArchitectureApplication.class, args);
+	}
+
+}
